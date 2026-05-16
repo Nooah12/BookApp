51a349b119d8dd49d50d1ab6c65747296fbe15d3
Noah Gordon <noah.o.gordon@gmail.com>
2026-05-16 15:52:09 +0200

    Add BookApp as regular folder

--- BookApi/obj/Debug/net9.0/BookApi.AssemblyInfo.cs
+++ BookApi/obj/Debug/net9.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d69e464084fd565bace3202337ed16839c7fb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
